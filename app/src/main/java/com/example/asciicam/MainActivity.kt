017170bd52a88269dab4d64fb29d6b9c9a721ec6
tugaserya <artempoznakov990@gmail.com>
2024-02-01 23:39:17 +0300

    new permision  and functions

--- app/src/main/java/com/example/asciicam/MainActivity.kt
+++ app/src/main/java/com/example/asciicam/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.example.asciicam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1
+    private val PICK_IMAGE_REQUEST = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectButton: Button = findViewById(R.id.selectButton)
+
+        selectButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE)
+            } else {
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                intent.type = "image/*"
+                startActivityForResult(intent, PICK_IMAGE_REQUEST)
+            }
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                    intent.type = "image/*"
+                    startActivityForResult(intent, PICK_IMAGE_REQUEST)
+                } else {
+
+                    Log.d("myTag", "Permission denied")
+                }
+                return
+            }
+            else -> {
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            val imageView: ImageView = findViewById(R.id.imageView)
+            val fileUri: Uri? = data?.data
+            try {
+                val inputStream = contentResolver.openInputStream(fileUri!!)
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                val imageConverter = ImageConverter()
+                if (bitmap != null) {
+                    val grayscaleBitmap: Bitmap = imageConverter.setGrayscaleBitmap(bitmap)
+                    imageView.setImageBitmap(grayscaleBitmap)
+                } else {
+                    Log.d("myTag","bitmap is null")
+                }
+            } catch (e: Exception) {
+                Log.e("myTag", "Error reading file: ${e.message}")
+            }
+        }
+        }
 }

f25493279539257830c705667dc38a8ae72d26f3
tugaserya <artempoznakov990@gmail.com>
2024-02-02 18:57:26 +0300

    refactor and delete some permishions

--- app/src/main/java/com/example/asciicam/MainActivity.kt
+++ app/src/main/java/com/example/asciicam/MainActivity.kt
@@ -1,23 +1,17 @@
 package com.example.asciicam
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1
-    private val PICK_IMAGE_REQUEST = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,51 +20,33 @@ class MainActivity : AppCompatActivity() {
         val selectButton: Button = findViewById(R.id.selectButton)
 
         selectButton.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE)
-            } else {
-                val intent = Intent(Intent.ACTION_GET_CONTENT)
-                intent.type = "image/*"
-                startActivityForResult(intent, PICK_IMAGE_REQUEST)
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, 1)
             }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    val intent = Intent(Intent.ACTION_GET_CONTENT)
-                    intent.type = "image/*"
-                    startActivityForResult(intent, PICK_IMAGE_REQUEST)
-                } else {
-
-                    Log.d("myTag", "Permission denied")
-                }
-                return
-            }
-            else -> {
-            }
-        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
-            val imageView: ImageView = findViewById(R.id.imageView)
             val fileUri: Uri? = data?.data
             try {
+                val textView = findViewById<TextView>(R.id.textView)
+                val stringBuilder = StringBuilder()
                 val inputStream = contentResolver.openInputStream(fileUri!!)
                 val bitmap = BitmapFactory.decodeStream(inputStream)
                 val imageConverter = ImageConverter()
+
                 if (bitmap != null) {
-                    val grayscaleBitmap: Bitmap = imageConverter.setGrayscaleBitmap(bitmap)
-                    imageView.setImageBitmap(grayscaleBitmap)
+                    val resizeBitmap: Bitmap = Bitmap.createScaledBitmap(bitmap, 50, 50, false)
+                    val grayscaleBitmap: Bitmap = imageConverter.setGrayscaleBitmap(resizeBitmap)
+                    val asciiArt: Array<Array<String>> = imageConverter.convertToASCII(grayscaleBitmap)
+                    for (row in asciiArt) {
+                        for (value in row) {
+                            stringBuilder.append("$value\t")
+                        }
+                        stringBuilder.append("\n")
+                    }
+                    textView.text = stringBuilder.toString()
                 } else {
                     Log.d("myTag","bitmap is null")
                 }
@@ -78,5 +54,5 @@ class MainActivity : AppCompatActivity() {
                 Log.e("myTag", "Error reading file: ${e.message}")
             }
         }
-        }
-}
+    }
+

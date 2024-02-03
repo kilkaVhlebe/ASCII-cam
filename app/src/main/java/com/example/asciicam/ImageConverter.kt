68bfc26a90a58769f2b8cb1f3dad74b43fceed47
tugaserya <artempoznakov990@gmail.com>
2024-02-03 10:44:56 +0300

    Clean up

--- app/src/main/java/com/example/asciicam/ImageConverter.kt
+++ app/src/main/java/com/example/asciicam/ImageConverter.kt
@@ -29,6 +29,7 @@ class ImageConverter {
         val width = bitmap.width
         val height = bitmap.height
         val asciiArt = Array(height) { Array(width) { "" } }
+        val SymbolsArr = arrayOf("@", "#", "S", "%", "?", "*", "+", ";", ":", ",", ".")
         for (x in 0 until width) {
             for (y in 0 until height) {
                 val pixel = bitmap.getPixel(x, y)
@@ -37,26 +38,21 @@ class ImageConverter {
                 val blue = Color.blue(pixel)
                 val brightness = (blue + green + red) / 3.0
                 when (brightness) {
-                    in 0.0..23.18 -> asciiArt[y][x] = matrixInput(0)
-                    in 23.19..46.3618 -> asciiArt[y][x] = matrixInput(1)
-                    in 46.3619..69.5418 -> asciiArt[y][x] = matrixInput(2)
-                    in 69.5418..92.7218 -> asciiArt[y][x] = matrixInput(3)
-                    in 92.7219..115.9018 -> asciiArt[y][x] = matrixInput(4)
-                    in 115.9019..139.0818 -> asciiArt[y][x] = matrixInput(5)
-                    in 139.0819..162.2618 -> asciiArt[y][x] = matrixInput(6)
-                    in 162.2619..185.4418 -> asciiArt[y][x] = matrixInput(7)
-                    in 185.4419..208.6218 -> asciiArt[y][x] = matrixInput(8)
-                    in 208.6219..231.8018 -> asciiArt[y][x] = matrixInput(9)
-                    in 231.8019..255.0 -> asciiArt[y][x] = matrixInput(10)
+                    in 0.0..23.18 -> asciiArt[y][x] = SymbolsArr[0]
+                    in 23.19..46.3618 -> asciiArt[y][x] = SymbolsArr[1]
+                    in 46.3619..69.5418 -> asciiArt[y][x] = SymbolsArr[2]
+                    in 69.5418..92.7218 -> asciiArt[y][x] = SymbolsArr[3]
+                    in 92.7219..115.9018 -> asciiArt[y][x] = SymbolsArr[4]
+                    in 115.9019..139.0818 -> asciiArt[y][x] = SymbolsArr[5]
+                    in 139.0819..162.2618 -> asciiArt[y][x] = SymbolsArr[6]
+                    in 162.2619..185.4418 -> asciiArt[y][x] = SymbolsArr[7]
+                    in 185.4419..208.6218 -> asciiArt[y][x] = SymbolsArr[8]
+                    in 208.6219..231.8018 -> asciiArt[y][x] = SymbolsArr[9]
+                    in 231.8019..255.0 -> asciiArt[y][x] = SymbolsArr[10]
                 }
             }
         }
         return asciiArt
-    }
-
-    fun matrixInput(index: Int): String {
-        val SymbolsArr = arrayOf("@", "#", "S", "%", "?", "*", "+", ";", ":", ",", ".")
-        return SymbolsArr[index]
     }
 }
 

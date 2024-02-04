5f2ee9944af71f11c7b3a75f5cfc448c97c08f14
tugaserya <artempoznakov990@gmail.com>
2024-02-04 20:41:13 +0300

    refactor and new progress bar if image is long converting

--- app/src/main/java/com/example/asciicam/ImageConverter.kt
+++ app/src/main/java/com/example/asciicam/ImageConverter.kt
@@ -19,9 +19,11 @@ class ImageConverter {
     fun setGrayscaleBitmap(bitmap: Bitmap): Bitmap {
         val grayscaleBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
         val paint = Paint()
+
         paint.setColorFilter(filter)
         val canvas = Canvas(grayscaleBitmap)
         canvas.drawBitmap(grayscaleBitmap, 0f, 0f, paint)
+
         return grayscaleBitmap
     }
 
@@ -29,30 +31,30 @@ class ImageConverter {
         val width = bitmap.width
         val height = bitmap.height
         val asciiArt = Array(height) { Array(width) { "" } }
-        val SymbolsArr = arrayOf("@", "#", "S", "%", "?", "*", "+", ";", ":", ",", ".")
+        val symbolsArr = arrayOf("@", "#", "S", "%", "?", "*", "+", ";", ":", ",", ".")
+
         for (x in 0 until width) {
             for (y in 0 until height) {
                 val pixel = bitmap.getPixel(x, y)
                 val red = Color.red(pixel)
                 val green = Color.green(pixel)
                 val blue = Color.blue(pixel)
-                val brightness = (blue + green + red) / 3.0
-                when (brightness) {
-                    in 0.0..23.18 -> asciiArt[y][x] = SymbolsArr[0]
-                    in 23.19..46.3618 -> asciiArt[y][x] = SymbolsArr[1]
-                    in 46.3619..69.5418 -> asciiArt[y][x] = SymbolsArr[2]
-                    in 69.5418..92.7218 -> asciiArt[y][x] = SymbolsArr[3]
-                    in 92.7219..115.9018 -> asciiArt[y][x] = SymbolsArr[4]
-                    in 115.9019..139.0818 -> asciiArt[y][x] = SymbolsArr[5]
-                    in 139.0819..162.2618 -> asciiArt[y][x] = SymbolsArr[6]
-                    in 162.2619..185.4418 -> asciiArt[y][x] = SymbolsArr[7]
-                    in 185.4419..208.6218 -> asciiArt[y][x] = SymbolsArr[8]
-                    in 208.6219..231.8018 -> asciiArt[y][x] = SymbolsArr[9]
-                    in 231.8019..255.0 -> asciiArt[y][x] = SymbolsArr[10]
+                when ((blue + green + red) / 3.0) {
+                    in 0.0..23.18 -> asciiArt[y][x] = symbolsArr[0]
+                    in 23.19..46.3618 -> asciiArt[y][x] = symbolsArr[1]
+                    in 46.3619..69.5418 -> asciiArt[y][x] = symbolsArr[2]
+                    in 69.5418..92.7218 -> asciiArt[y][x] = symbolsArr[3]
+                    in 92.7219..115.9018 -> asciiArt[y][x] = symbolsArr[4]
+                    in 115.9019..139.0818 -> asciiArt[y][x] = symbolsArr[5]
+                    in 139.0819..162.2618 -> asciiArt[y][x] = symbolsArr[6]
+                    in 162.2619..185.4418 -> asciiArt[y][x] = symbolsArr[7]
+                    in 185.4419..208.6218 -> asciiArt[y][x] = symbolsArr[8]
+                    in 208.6219..231.8018 -> asciiArt[y][x] = symbolsArr[9]
+                    in 231.8019..255.0 -> asciiArt[y][x] = symbolsArr[10]
                 }
             }
         }
+
         return asciiArt
     }
 }
-
--- app/src/main/java/com/example/asciicam/MainActivity.kt
+++ app/src/main/java/com/example/asciicam/MainActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.awaitAll
+import java.util.Timer
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,41 +29,52 @@ class MainActivity : AppCompatActivity() {
         selectButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "image/*"
+            @Suppress("DEPRECATION")
             startActivityForResult(intent, 1)
-            }
+        }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-            val fileUri: Uri? = data?.data
             try {
                 val textView = findViewById<TextView>(R.id.textView)
-                val stringBuilder = StringBuilder()
+                val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+                val fileUri: Uri? = data?.data
                 val inputStream = contentResolver.openInputStream(fileUri!!)
                 val bitmap = BitmapFactory.decodeStream(inputStream)
+
+                val displayMetrics = DisplayMetrics()
+                val stringBuilder = StringBuilder()
                 val imageConverter = ImageConverter()
 
                 if (bitmap != null) {
-                    val displayMetrics = DisplayMetrics()
+                    @Suppress("DEPRECATION")
                     windowManager.defaultDisplay.getMetrics(displayMetrics)
                     val height = displayMetrics.heightPixels
                     val width = displayMetrics.widthPixels
+
                     val resizeBitmap: Bitmap = Bitmap.createScaledBitmap(bitmap, width/20, height/17, false)
                     val grayscaleBitmap: Bitmap = imageConverter.setGrayscaleBitmap(resizeBitmap)
                     val asciiArt: Array<Array<String>> = imageConverter.convertToASCII(grayscaleBitmap)
+
+                    progressBar.visibility = View.VISIBLE
+
                     for (row in asciiArt) {
                         for (value in row) {
                             stringBuilder.append("$value\t")
                         }
                         stringBuilder.append("\n")
                     }
+
+                    progressBar.visibility = View.INVISIBLE
+
                     textView.text = stringBuilder.toString()
-                } else {
-                    Log.d("myTag","bitmap is null")
                 }
+
             } catch (e: Exception) {
                 Log.e("myTag", "Error reading file: ${e.message}")
             }
         }
     }
-
